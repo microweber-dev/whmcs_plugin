5f4963ccf8ce7c57520b555d1842ab014d8caf0c
Peter Ivanov <peter@microweber.com>
2019-05-09 15:28:56 +0300

    updates

--- order/embed.js
+++ order/embed.js
@@ -1,58 +1,50 @@
 (function () {
+    var mlt = 'Multiple embed instances not allowed';
+    if(window['mw-domain-search-iframe-embed']) {
+        console.warn(mlt);
+        return;
+    }
+    window['mw-domain-search-iframe-embed'] = true;
 
+    if(document.querySelectorAll('script[src*="/microweber_addon/"]').length > 1) {
+        console.warn(mlt);
+        return;
+    }
 
-    var script = document.getElementById("domain-search-iframe-js");
-    var script_src = script.src;
+    var $ = function(a){ return document.querySelector(a) };
 
-    var script_src_root = script_src.split('/');
+    var script = $('script[src*="/microweber_addon/"]');
 
+    if (!script) {
+        return;
+    }
 
-    script_src_root.pop()
-
-    script_src_root = script_src_root.join('/')+'/'
-
-
-
+    var path = script.src.split('/');
+        path.pop()
+        path = path.join('/');
+    var $params = script.src.split('?')[1] || '';
+    _params = $params.split('&');
+    var params = {};
+    for( var i = 0; i < _params.length; i++ ) {
+        var item = _params[i].split('=');
+        params[decodeURIComponent(item[0]).trim()] = decodeURIComponent(item[1]).trim();
+    }
 
     var iframe = document.createElement("iframe");
-    iframe.src = script_src_root+'index.php';
+    iframe.src = path + '/index.php?' + $params;
     iframe.id = 'domain-search-iframe';
     iframe.style.width = "100%";
-    iframe.style.height = "100px%";
-    iframe.onload = function(){
-  //      iframe.contentWindow.document.body.style.margin = 5+'px';
-    //    iframe.style.height = (iframe.contentWindow.document.body.clientHeight+10)+"px";
-    }
+    iframe.style.height = "100px";
+    iframe.scrolling = 'no';
     iframe.frameBorder = 0;
     iframe.allowtransparency = 1;
-    document.getElementById("domain-search-iframe-js").parentNode.insertBefore(iframe,script.nextSibling );
 
+    script.parentNode.insertBefore(iframe, script.nextSibling);
 
-})();
-
-
-
-$(document).ready(function () {
-    // Setup event listener
-    var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-    var eventer = window[eventMethod];
-    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-
-    // Listen for event
-    eventer(messageEvent, function (e) {
-
-        // Check that message being passed is the documentHeight
-        if ((typeof e.data === 'string') && (e.data.indexOf('documentHeight:') > -1)) {
-
-            // Split string from identifier
+    addEventListener('message', function (e) {
+        if (typeof e.data === 'string' && e.data.indexOf('documentHeight:') > -1) {
             var height = e.data.split('documentHeight:')[1];
-
-            $('#domain-search-iframe').css({'height': height + 'px'})
-            // do stuff with the height
-
+            $('#domain-search-iframe').style.height = height + 'px';
         }
     }, false);
-
-});
-
-
+})();
