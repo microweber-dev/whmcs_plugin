91953d758a565f39a6d9375b974390d271f5f43f
Petko Yovchevski <pyovchevski@gmail.com>
2019-08-09 14:58:56 +0300

    updates

--- src/views/index.php
+++ src/views/index.php
@@ -86,85 +86,204 @@ $enabled_templates = $manager->hosting->get_enabled_templates('return_mode=simpl
 
     ?>
 
-    <form method="post">
-        <input type="hidden" name="function" value="save"/>
+    <style>
+        .project {
+            margin-bottom: 30px;
+            vertical-align: top;
+            margin-right: 30px;
+            float: left;
+            cursor: pointer;
+            width: 100%;
+        }
+
+        .project figure {
+            position: relative;
+            display: inline-block;
+            height: 200px;
+            overflow: hidden;
+        }
+
+        .project figure img {
+            width: 100%;
+        }
+
+        .btn-warning bnt-action {
+            margin: 0% 0% auto;
+        }
+
+        figcaption .project-details {
+            display: block;
+            font-size: 16px;
+            /*line-height: 33px;*/
+            color: #000;
+            /*height: 27px;*/
+            width: 100%;
+            margin: 0 auto 5px auto;
+            /*margin-bottom: 5px;*/
+            overflow: hidden;
+        }
+
+        .project figure:hover figcaption {
+            background: #d81e05;
+        }
+
+        .project figure:hover figcaption .project-details {
+            color: #fff;
+        }
+
+        figcaption .project-price {
+            position: absolute;
+            right: 15px;
+            top: 12px;
+            font-size: 22px;
+            text-align: right;
+            margin-top: 8px;
+            letter-spacing: -1px;
+            -webkit-font-smoothing: antialiased;
+        }
+
+        figcaption .project-creator {
+            font-size: 13px;
+            color: #545454;
+            display: block;
+        }
+
+        figcaption .project-creator {
+            font-size: 13px;
+            color: #545454;
+            display: block;
+        }
+
+        .project .actions button {
+            padding: 13px 20px;
+            font-size: 16px;
+            top: 32%;
+            position: absolute;
+            left: 50%;
+            width: 90%;
+            margin-left: -45%;
+            line-height: 18px;
+            letter-spacing: 1px;
+        }
+
+        .project figure:hover .actions {
+            background-color: rgba(29, 29, 29, 1);
+            font-size: 2em;
+            font-weight: 700;
+        }
+
+        .project .actions {
+            display: block;
+            position: relative;
+            z-index: 1;
+            opacity: 1;
+            background-color: rgba(29, 29, 29, .9);
+            -ms-transition: all .2s ease-out;
+            -webkit-transition: all .2s ease-out;
+            -moz-transition: all .2s ease-out;
+            -o-transition: all .2s ease-out;
+            transition: all .2s ease-out;
+            color: white;
+            font-size: 14px;
+            padding: 2px 10px;
+            font-weight: bold;
+            text-align: center;
+        }
 
 
-        <div class="form-group col-xs-10 col-sm-6 col-md-6 col-lg-6">
-            <h1>Select hosting plans to offer website builder</h1>
+    </style>
 
-            <?php foreach ($hosting as $plan) : ?>
+    <div class="container">
+        <form method="post">
+            <input type="hidden" name="function" value="save"/>
 
-                <div class="form-group">
+            <h1 style="font-weight: bold; margin-bottom: 10px;">Select Hosting plans</h1>
+            <p>Select the hosting plans you want to install by default Microweber CMS.</p>
 
-                    <div class="checkbox">
-                        <label><input <?php if (isset($plan['has_website_builder']) and $plan['has_website_builder']) { ?> checked  <?php } ?>
-                                    name="selected_hosting_plans[]" type="checkbox"
-                                    value="<?php print $plan['id'] ?>"><?php print $plan['name'] ?>
-                        </label>
+            <hr class="divider-title">
+
+            <div class="row">
+                <?php foreach ($hosting as $plan) : ?>
+                    <div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label><input <?php if (isset($plan['has_website_builder']) and $plan['has_website_builder']) { ?> checked  <?php } ?> name="selected_hosting_plans[]" type="checkbox" value="<?php print $plan['id'] ?>"><?php print $plan['name'] ?></label>
+                            </div>
+                        </div>
                     </div>
+                <?php endforeach; ?>
+            </div>
 
-                </div>
-            <?php endforeach; ?>
+            <br/>
+            <br/>
+            <br/>
 
+            <h1 style="font-weight: bold; margin-bottom: 10px;">Select Templates</h1>
+            <p>Select templates which will be allowable to install by your clients. This templates are available only with <a href="https://microweber.org/modules/mw-internal/white_label" target="_blank">White Label Hosting Pro License</a>.</p>
 
-        </div>
-
-
-        <div class="form-group col-xs-10 col-sm-6 col-md-6 col-lg-6">
-            <h1>Select templates</h1>
-
-
-            <?php foreach ($templates as $template) : ?>
-                <?php
-
-                $item = $template['latest_version'];
-                $screenshot = '';
-                if (isset($item['extra']) and isset($item['extra']['_meta']) and isset($item['extra']['_meta']['screenshot'])) {
-                    $screenshot = $item['extra']['_meta']['screenshot'];
-                }
-
-
-                ?>
-                <div class="form-group">
-
-                    <div class="checkbox">
-                        <label>
-                            <div class="template-bg-img"
-                                 style=" background-image: url('<?php print $screenshot ?>');"></div>
-
-                            <input <?php if (in_array($item['target-dir'], $enabled_templates)) { ?> checked  <?php } ?>
-                                    type="checkbox" name="selected_templates[]"
-                                    value="<?php print $item['target-dir'] ?>"><?php print $item['description'] ?>
-                        </label>
+            <hr class="divider-title">
+            <div class="row">
+                <div class="col-xs-12">
+                    <script>
+                        $(document).ready(function () {
+                            $('.js-select-all-templates').on('click', function () {
+                                $('.js-template').each(function () {
+                                    $(this).find('input').prop("checked", true);
+                                })
+                            })
+                            $('.js-unselect-all-templates').on('click', function () {
+                                $('.js-template').each(function () {
+                                    $(this).find('input').prop("checked", false);
+                                })
+                            })
+                        })
+                    </script>
+                    <div class="pull-right" style="height: 50px;">
+                        <button type="button" class="js-select-all-templates btn btn-sm">Select all</button>
+                        <button type="button" class="js-unselect-all-templates btn btn-sm">Unselect all</button>
                     </div>
-
                 </div>
-            <?php endforeach; ?>
+                <?php foreach ($templates as $template) : ?>
+                    <?php
+                    $item = $template['latest_version'];
+                    $screenshot = '';
+                    if (isset($item['extra']) and isset($item['extra']['_meta']) and isset($item['extra']['_meta']['screenshot'])) {
+                        $screenshot = $item['extra']['_meta']['screenshot'];
+                    }
+                    ?>
+
+                    <div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 js-template">
+                        <div class="project">
+                            <figure class="img-responsive">
+                                <img src="<?php print $screenshot ?>">
+                            </figure>
+                            <span class="actions">
+                                 <span class="project-details">
+                                 <div class="checkbox">
+                                   <label><input <?php if (in_array($item['target-dir'], $enabled_templates)) { ?> checked  <?php } ?> type="checkbox" name="selected_templates[]" value="<?php print $item['target-dir'] ?>"> <strong><?php print $item['description'] ?></strong></label>
+                                  </div>
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
 
 
-        </div>
+            <button type="submit" class="btn btn-default">Save changes</button>
+        </form>
+    </div>
 
 
-        <button type="submit">Save</button>
+    <!--    <pre>-->
+    <?php // print_r($settings_render) ?>
+    <?php //print_r($settings) ?>
+    <?php // print_r($templates) ?>
+    <?php // print_r($params) ?>
+    <?php //print_r($enabled_templates) ?>
+    <!--   </pre>-->
 
-    </form>
-
-
-    hi
-
-
-    <pre>
-        <?php // print_r($settings_render) ?>
-        <?php //print_r($settings) ?>
-        <?php // print_r($templates) ?>
-        <?php // print_r($params) ?>
-        <?php print_r($enabled_templates) ?>
-   </pre>
-
-
-
-    <?php include_once "embed_codes.php"; ?>
+    <?php //include_once "embed_codes.php"; ?>
 
 </div>
 
