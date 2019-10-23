e3b0dff559865d142f2108a8216ca8c7c5d5025c
b.slaveykov <b.slaveykov@radioactive.bg>
2019-10-23 13:16:46 +0300

    update

--- order/steps/domains.php
+++
@@ -1,5 +0,0 @@
-<?php if (isset($_GET['style'])) {
-    include(dirname(__DIR__) . '/templates/' . htmlspecialchars($_GET['style']) . '/domains/index.php');
-} else {
-    include(dirname(__DIR__) . '/templates/default/domains/index.php');
-}
--- order/steps/templates.php
+++
@@ -1,5 +0,0 @@
-<?php if (isset($_GET['style'])) {
-    include(dirname(__DIR__) . '/templates/' . htmlspecialchars($_GET['style']) . '/templates/index.php');
-} else {
-    include(dirname(__DIR__) . '/templates/default/templates/index.php');
-}
