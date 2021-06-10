c03407024f971f35aba46a53244eacc3b3c5af34
Peter Ivanov <peter@microweber.com>
2021-06-10 17:22:16 +0300

    update

--- modules/addons/microweber_addon/src/views/embed_codes.php
+++ modules/addons/microweber_addon/src/views/embed_codes.php
@@ -5,10 +5,17 @@
 
     You can use this embed code on any website to show domain search field.
 
+    Target top
 <pre >
 &#x3C;script  crossorigin="anonymous" src=&#x22;<?php print $config['SystemURL'] ?>/modules/addons/microweber_addon/order/embed.js?style=default&target=top&#x22; id=&#x22;domain-search-iframe-js&#x22;&#x3E;&#x3C;/script&#x3E;
 </pre>
 
+
+    Without target top
+
+<pre >
+&#x3C;script  crossorigin="anonymous" src=&#x22;<?php print $config['SystemURL'] ?>/modules/addons/microweber_addon/order/embed.js&#x22; id=&#x22;domain-search-iframe-js&#x22;&#x3E;&#x3C;/script&#x3E;
+</pre>
 
 
 Iframe
