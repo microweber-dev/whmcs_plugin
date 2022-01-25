7588921fdfee84275a34a61136be52761f63cb90
Bozhidar Slaveykov <bobi@microweber.com>
2022-01-25 17:13:21 +0200

    Update whmcs_products_checker.js

--- modules/addons/microweber_addon/whmcs_products_checker.js
+++ modules/addons/microweber_addon/whmcs_products_checker.js
@@ -3,16 +3,51 @@ $(document).ready(function() {
 
         var URL = "index.php?m=microweber_addon&ajax=1&function=service_check&service_id=" + encodeURI($(item).data('service-id'));
 
-        $.ajax({
-            contentType: 'application/json',
-            dataType: 'json',
-            url: URL,
-            cache: false,
-            type: "POST",
-            success: function (response) {
+        lazyLoadAjaxManager.addRequest(URL); 
 
-
-            }
-        });
     });
 });
+
+var lazyLoadAjaxManager = (function() {
+
+    const MAX_PARALLEL_CALL = 1;
+    var queue = []; //to store the URL
+    var activeCall = 0;
+
+    function queueRequest(url) {
+        queue.push(url)
+        checkQueue()
+    }
+
+    function onPromiseCompleteOrFailure() {
+        activeCall--
+        checkQueue()
+    }
+
+    function checkQueue() {
+        if (queue.length && activeCall <= MAX_PARALLEL_CALL) {
+            let url = queue.shift()
+            if (!url) {
+                return
+            }
+
+            activeCall++
+
+            fetch(url)
+                .then(res => res.json())
+                .then(response => {
+                    onPromiseCompleteOrFailure()
+                    //TODO Write your custom logic here
+                    console.log('Success:', JSON.stringify(response))
+                })
+                .catch(error => {
+                    onPromiseCompleteOrFailure()
+                    console.error('Error:', error)
+                })
+        }
+    }
+
+    return {
+        addRequest: queueRequest,
+    }
+})();
