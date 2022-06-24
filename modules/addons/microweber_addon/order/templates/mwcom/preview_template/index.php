2e6b2d736d207cf0212caf6b232095e503d006bd
Peter Ivanov <peter@microweber.com>
2022-06-24 11:50:17 +0300

    update

---
+++ modules/addons/microweber_addon/order/templates/mwcom/preview_template/index.php
@@ -0,0 +1,147 @@
+<?php include 'partials/header.php'; ?>
+
+<?php if (isset($_REQUEST['template'])): ?>
+    <?php
+    $template = get_template_by_config_option_id($_REQUEST['template']);
+
+
+    $homepage = 'https://microweber.com';
+    $preview_url = 'https://microweber.com';
+    if (!empty($template->preview_url)) {
+        $preview_url = $template->preview_url;
+    }
+    if (!empty($template->homepage_url)) {
+        $homepage = $template->homepage_url;
+    }
+
+    ?>
+
+    <?php include dirname(dirname(dirname(__DIR__))) . '/params.php'; ?>
+
+    <style>
+        .btn-primary {
+            color:#fff !important;
+            background-color: #1062fe;
+            border-color: #1062fe;
+
+        }
+
+
+    </style>
+
+    <div class="container"  >
+    <div class="preview-navbar">
+        <div class="left logo">
+            <a href="javascript:;" onclick="window.history.back()" class="btn btn-default"><i class="fa fa-chevron-left"></i></a>
+        </div>
+
+        <?php
+        /*<div class="screens">
+            <button onclick="selectDevice('desktop')" type="button">
+                <svg width="28" height="22" viewBox="0 0 28 22" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="#fff" fill-rule="evenodd">
+                        <path d="M11 18h1v4h-1z"></path>
+                        <path d="M9 21h10v1H9z"></path>
+                        <path d="M16 18h1v4h-1z"></path>
+                        <path d="M1 3v13c0 1.11.891 2 1.996 2h22.008A2.004 2.004 0 0 0 27 16V3c0-1.11-.891-2-1.996-2H2.996A2.004 2.004 0 0 0 1 3zM0 3c0-1.657 1.35-3 2.996-3h22.008A2.994 2.994 0 0 1 28 3v13c0 1.657-1.35 3-2.996 3H2.996A2.994 2.994 0 0 1 0 16V3z"></path>
+                    </g>
+                </svg>
+            </button>
+            <button onclick="selectDevice('tablet')" type="button">
+                <svg width="20" height="28" viewBox="0 0 20 28" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="#fff" fill-rule="evenodd">
+                        <path d="M1 2.996v22.008C1 26.1 1.897 27 2.994 27h14.012c1.1 0 1.994-.895 1.994-1.996V2.996A2.001 2.001 0 0 0 17.006 1H2.994C1.894 1 1 1.895 1 2.996zm-1 0A2.997 2.997 0 0 1 2.994 0h14.012A3.001 3.001 0 0 1 20 2.996v22.008A2.997 2.997 0 0 1 17.006 28H2.994A3.001 3.001 0 0 1 0 25.004V2.996z"></path>
+                        <path d="M9 23h2v2H9z"></path>
+                    </g>
+                </svg>
+            </button>
+            <button onclick="selectDevice('phone')" type="button">
+                <svg width="12" height="22" viewBox="0 0 12 22" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="#fff" fill-rule="evenodd">
+                        <path d="M1 3.001V19C1 20.105 1.894 21 2.997 21h6.006A2 2 0 0 0 11 18.999V3A1.999 1.999 0 0 0 9.003 1H2.997A2 2 0 0 0 1 3.001zm-1 0A3 3 0 0 1 2.997 0h6.006A2.999 2.999 0 0 1 12 3.001V19A3 3 0 0 1 9.003 22H2.997A2.999 2.999 0 0 1 0 18.999V3z"></path>
+                        <path d="M5 18h2v2H5z"></path>
+                    </g>
+                </svg>
+            </button>
+        </div>*/
+
+        ?>
+
+        <div class="right buttons">
+
+            <form method="post" action="<?php echo $current_url ?>" class="clearfix">
+                <input type="hidden" value="false" name="template_view"/>
+                <input type="hidden" value="1" name="start_with_template"/>
+
+                <?php include dirname(dirname(dirname(__DIR__))) . '/params_fields.php';?>
+
+                <a href="<?php print $preview_url; ?>" target="_blank">
+                    <span>Preview site</span>
+                </a>
+
+                <button class="btn btn-primary" onclick="submitForPreview(this.parentNode);">
+                    <span>Start with this template</span>
+                </button>
+
+            </form>
+        </div>
+    </div>
+
+    <?php if ($preview_url): ?>
+    <div class="loading-iframe-loader"> </div>
+<style>
+        .loading-iframe-loader {
+        border: 5px solid #bae7e7;
+        border-radius: 50%;
+        border-top: 5px solid #3498db;
+        width: 40px;
+        height: 40px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin: -20px 0 0 -20px;
+        -webkit-animation: spin 2s linear infinite; / Safari /
+        animation: spin 2s linear infinite;
+        }
+
+
+        @-webkit-keyframes spin {
+        0% { -webkit-transform: rotate(0deg); }
+        100% { -webkit-transform: rotate(360deg); }
+        }
+
+        @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+        }
+</style>
+
+
+    <iframe id="template-demo-iframe" src="<?php print $preview_url; ?>"
+            name="preview-frame" frameborder="0"
+            noresize="noresize" data-view="fullScreenPreview"
+            referrerpolicy="no-referrer"
+            height="1500px" width="100%"
+            scrolling="yes"
+            style="height: 1000px"
+            onload="$('.loading-iframe-loader').hide()"
+            onerror="$('.loading-iframe-loader').hide()"
+            allow="geolocation 'self'; autoplay 'self'">Loading....
+    </iframe>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function(event) {
+            if (window.parent != window.self) {
+            //     $('#template-demo-iframe').css('height', (top.innerHeight - $('.preview-navbar').outerHeight()));
+            } else {
+             //   $('#template-demo-iframe').css('height', (self.innerHeight - $('.preview-navbar').outerHeight()));
+            }
+        });
+    </script>
+    <?php endif; ?>
+
+
+    </div>
+<?php endif; ?>
+
+<?php include 'partials/footer.php'; ?>
---
+++ modules/addons/microweber_addon/order/templates/mwcom/templates/index.php
@@ -0,0 +1,106 @@
+<?php include 'partials/header.php'; ?>
+<style>
+    .templates {
+        padding: 60px 0;
+        background: #fff;
+
+    }
+
+    .templates h3 {
+        color: #2b2b2b;
+        font-size: 14px;
+        font-weight: bold;
+    }
+
+    .templates p {
+        color: #2b2b2b;
+        font-size: 13px;
+
+    }
+
+    .templates .container {
+        background: #fff;
+    }
+
+    .templates a {
+        text-decoration: none !important;
+    }
+
+    .templates .template {
+        width: 420px;
+        height: 350px;
+        -webkit-background-size: contain;
+        background-size: contain;
+        background-repeat: no-repeat;
+        display: inline-block;
+        position: relative;
+        overflow: hidden;
+        transition: all 5s;
+        background-size: 100% auto;
+        background-repeat: no-repeat;
+        background-position: center top;
+        margin: 30px 0 0 0;
+        border: 2px solid #f5f5f5;
+        max-width: 100%;
+    }
+
+    .templates .template:hover {
+        background-position: center bottom;
+    }
+
+</style>
+<?php
+$templates = get_enabled_templates();
+
+ ?>
+<?php if (empty((array) $templates)): ?>
+    <div class="alert alert-danger" role="alert">
+        No templates are enabled from the admin panel
+    </div>
+ <?php endif; ?>
+
+<div class="templates">
+    <div class="container">
+        <?php if (isset($_GET['target']) AND $_GET['target'] == '_top'): ?>
+            <div class="row">
+                <div class="col-md-12 text-center"><h1>Select a template</h1></div>
+            </div>
+        <?php endif; ?>
+
+        <div class="row">
+            <?php if ($templates): ?>
+                <?php foreach ($templates as $template): ?>
+
+                    <?php
+                    $template_id = $template->config_option_id;
+                    $config_gid = $template->config_option_group_id;
+                    ?>
+
+                    <?php include dirname(dirname(dirname(__DIR__))) . '/params.php'; ?>
+
+                    <div class="col-md-4 col-xs-6">
+                        <form method="post" action="<?php echo $current_url ?>" <?php if (isset($_GET['target']) AND $_GET['target'] == 'top'): ?> target="_top"<?php endif; ?> class="clearfix">
+
+                            <?php include dirname(dirname(dirname(__DIR__))) . '/params_fields.php'; ?>
+
+                            <input type="hidden" value="true" name="template_view"/>
+
+                            <a href="javascript:;" onclick="parentNode.submit();">
+                                <div class="template" style="background-image: url('<?php echo $template->screenshot_url; ?>');"></div>
+                                <h3><?php echo $template->preview_name; ?></h3>
+                            </a>
+
+                        </form>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function () {
+        scroll_iframe_to_parent();
+    });
+</script>
+<?php include 'partials/footer.php'; ?>
