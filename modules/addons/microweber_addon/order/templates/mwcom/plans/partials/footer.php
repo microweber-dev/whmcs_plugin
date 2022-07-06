f0ff9df494c87351b872fb0ac1bcf93f505ba23a
Peter Ivanov <peter@microweber.com>
2022-07-06 10:54:19 +0300

    update

--- modules/addons/microweber_addon/order/templates/mwcom/plans/partials/footer.php
+++
@@ -1,8 +0,0 @@
-            <script src="<?php echo site_url(); ?>/modules/addons/microweber_addon/scripts.js"></script>
-
-            <!-- Plugins -->
-            <script src="<?php echo site_url(); ?>/modules/addons/microweber_addon/order/assets/plugins/bootstrap-3.3.7/js/bootstrap.min.js"></script>
-            <!--<script src="--><?php //echo site_url(); ?><!--/modules/addons/microweber_addon/order/templates/default/plans/assets/js/main.js"></script>-->
-        </div>
-    </body>
-</html>
