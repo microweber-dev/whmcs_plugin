440a88181e0fbe9807a6acad706e133fd7c9b748
Nikolay <rachkuw@gmail.com>
2022-07-20 14:54:53 +0300

    update

--- modules/addons/microweber_addon/order/templates/whmcs-order-process-style-2022/domains/index.php
+++ modules/addons/microweber_addon/order/templates/whmcs-order-process-style-2022/domains/index.php
@@ -302,10 +302,10 @@
 
                 var ajax_status_check_class = '';
                 var can_start_class = 'cant-start';
-                var item_status_span = '<span class="not-available-tag">Unavailable</span>';
+                var item_status_span = '<i class="availability-domain-checker-icon fa fa-close" ></i> <span class="not-available-tag availability-domain-checker-tag">Unavailable</span>';
 
                 if (item.status == 'available') {
-                    item_status_span = '<span class="domain-recommended-tag">Available</span>';
+                    item_status_span = '<i class="availability-domain-checker-icon fa fa-check" ></i> <span class="domain-recommended-tag availability-domain-checker-tag">Available</span>';
                     can_start_class = 'can-start';
                 }
 
@@ -315,7 +315,7 @@
                 }
 
                 if (item.is_free) {
-                    item_status_span = '<span class="domain-free-tag">Free</span>';
+                    item_status_span = '<i class="availability-domain-checker-icon fa fa-check"></i> <span class="domain-free-tag availability-domain-checker-tag">Free</span>';
                 }
                 var other_data = '';
                 <?php if(isset($_REQUEST['template_id'])): ?>
